1882bbb6566baf1ffee0ed32bb27b51bf54c4cba
Didrik Nathaniel LLoyd Aasland Skjelbred <141577441+DidrikGetacademy@users.noreply.github.com>
2024-02-28 15:14:59 +0100

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace FinancialSystemBackend_api_database
+{
+    public class User
+    { 
+      
+        
+        public bool IsOnline { get; set; }
+        public int id { get; set; }
+        public string Username { get; set; }
+        public string? Password { get; set; }
+
+      
+        public List<Savings> UserSavingProjects { get; set; }
+
+        public User()
+        {
+            UserSavingProjects = new List<Savings>();
+        }
+
+    }
+}
